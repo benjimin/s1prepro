5d2077588ce5a2e589c64c882c0f550a729ae987
Ben Lewis <ben.lewis@ga.gov.au>
2017-06-05 11:04:47 +1000

    refining bulk process

--- bulk.sh
+++ bulk.sh
@@ -6,6 +6,9 @@
 # For testing, try a random subset of the full input list:
 # $   sort -R full.list | head > subset.list 
 
+# For complete run:
+# $ /usr/bin/time ./bulk.sh full.list | tee master.log
+
 
 if [ "$#" != "1" ]; then # validate argument count
     echo Usage: . bulk.sh input_list.txt
@@ -17,10 +20,12 @@ else
 
     while read -r line; do
         short=$(echo $line | sed s:^.\*/:: | sed s:\.zip$::)
-        echo
+
         echo $short
 
         . prepro.sh $line $short.dim > $short.log 2>&1
+
+        [ -e $short.data ] && echo DONE || echo ABORT # check whether final output produced (not whether ok)
     done < $1
 
     date
